9312ad297f26ad48631bdff94c95f6ae6d3811c0
Pranav Mavle <156574461+Pranav8206@users.noreply.github.com>
2026-03-29 12:42:20 +0530

    Pranav (#13)
    * feat: initialize backend with MongoDB connection and setup scripts
    
    * update backend depedencies and add DB_NAME env variable
    
    * feat: add user controller and create folder structure for  middlewares, routes, and controllers, models with
    
    * refactor: rename avatar field to profilPic and remove unused fields from user schema
    
    * refactor: update app.js for improved error handling and route management; add auth middleware in auth.middleware.js
    
    * feat: add utility functions for error handling and cloud file uploads
    
    * feat: implement user registration and login functionality with JWT authentication
    
    * feat: add delete account and logout functionality to user controller
    
    * feat: add logout and delete account routes to user controller
    
    * feat: update user routes to remove redundant logout and delete account endpoints
    
    * chore: update dependencies and improve layout structure

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -6,12 +6,12 @@ import Footer from "@/components/Footer";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* Add bg-gray-50 and text-gray-900 here */}
-      <body className="min-h-screen bg-gray-50 text-gray-900 flex flex-col">
+      <body
+        className="min-h-screen bg-gray-50 text-gray-900 flex flex-col"
+        suppressHydrationWarning
+      >
         <Navbar />
-        <main className="grow pt-24 md:pt-32">
-          {children}
-        </main>
+        <main className="grow pt-24 md:pt-32">{children}</main>
         <Footer />
       </body>
     </html>
